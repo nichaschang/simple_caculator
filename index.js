82fbd7cf23e0e635283ff159c877c9ae9bdb5c96
nich <nichaschang@gmail.com>
2022-10-19 11:24:12 +0800

    edit logic

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//basic
 let num_objec = {
     current_value:null,
     next_value:null,
@@ -5,8 +6,10 @@ let num_objec = {
     is_points:false
 }
 
+// input value
 let display_num = document.querySelector('#answer');
 
+// 歸0歸0 AC type
 function reset()
 {
     num_objec = {
@@ -15,88 +18,92 @@ function reset()
         act:null,
         is_points:false
     }
+
     display_num.value = 0;
 }
 
+// 正負切換 change +-
 function reverse()
 {
     if(num_objec.current_value != null && num_objec.act == null)
     {
         num_objec.current_value *= -1;
         display_num.value = num_objec.current_value;
-    }else{
+    }
+    else{
         num_objec.next_value *= -1;
         display_num.value = num_objec.next_value;
     }
 }
 
+// 百分比 percent n*0.1
 function percent()
 {
     if(num_objec.current_value != null && num_objec.act == null)
     {
         num_objec.current_value *= 0.01;
         display_num.value = num_objec.current_value;
-    }else{
+    }
+    else{
         num_objec.next_value *= 0.01;
         display_num.value = num_objec.next_value;
     }
 }
 
-//加法
+//加法 plus
 function plus()
 {
     let max = Math.max(point_len(num_objec.current_value),point_len(num_objec.next_value));
-    let new_total = Math.floor(Number(num_objec.current_value) * Math.pow(10 , max)) + Math.floor(Number(num_objec.next_value) * Math.pow(10 , max))
+    let new_total = Math.floor(Number(num_objec.current_value) * Math.pow(10, max)) + Math.floor(Number(num_objec.next_value) * Math.pow(10, max))
     
-    num_objec.current_value = new_total / Math.pow(10 , max);
+    num_objec.current_value = new_total / Math.pow(10, max);
 }
 
-//減法
+//減法 minus
 function minus()
 {
     let max = Math.max(point_len(num_objec.current_value),point_len(num_objec.next_value));
-    let new_total = Math.floor(Number(num_objec.current_value) * Math.pow(10 , max)) - Math.floor(Number(num_objec.next_value) * Math.pow(10 , max))
+    let new_total = Math.floor(Number(num_objec.current_value) * Math.pow(10 , max)) - Math.floor(Number(num_objec.next_value) * Math.pow(10, max))
 
     num_objec.current_value = new_total / Math.pow(10 , max);
 }
 
-//乘法
-function multiply(){
+//乘法 multiply
+function multiply()
+{
     let max = Number(point_len(num_objec.current_value)) + Number(point_len(num_objec.next_value));
     let current_max = point_len(num_objec.current_value);
     let next_max = point_len(num_objec.next_value);
-    let new_total = Math.floor(Number(num_objec.current_value) * Math.pow(10 , Number(current_max))) * Math.floor(Number(num_objec.next_value) * Math.pow(10 , next_max))
+    let new_total = Math.floor(Number(num_objec.current_value) * Math.pow(10 , Number(current_max))) * Math.floor(Number(num_objec.next_value) * Math.pow(10, next_max))
 
     num_objec.current_value = new_total / Math.pow(10 , max);
 }
 
-//除法
-function devided(){
+//除法 multiply
+function devided()
+{
     let max = Math.max(Number(point_len(num_objec.current_value)), Number(point_len(num_objec.next_value)));
-    let new_total = Math.floor(Number(num_objec.current_value) * Math.pow(10 , Number(max))) / Math.floor(Number(num_objec.next_value) * Math.pow(10 , max))
+    let new_total = Math.floor(Number(num_objec.current_value) * Math.pow(10 , Number(max))) / Math.floor(Number(num_objec.next_value) * Math.pow(10, max))
 
     num_objec.current_value = new_total;
 }
 
-//切換小數點
+//切換小數點 add point
 function isPoint()
 {
     num_objec.is_points = true;
 }
 
-//數學符號
+//數學符號 add math symbols
 function symbolAct(str)
 {
-    if(num_objec.next_value != null && num_objec.act != null)
-    {
-        cal('symbolAct');
-    }
-
+    if(num_objec.next_value != null && num_objec.act != null) cal('symbolAct');
+    
     num_objec.act = str;
     num_objec.is_points = false;
 }
 
-//計算
+//計算 calculate the answer
 function cal(str = '')
 {
     let math_act = num_objec.act;
@@ -115,18 +122,14 @@ function cal(str = '')
             devided();
             break;
     }
+
     num_objec.next_value = null;
     num_objec.is_points = false;
-    
-    if(str != 'symbolAct'){
-        num_objec.act = null;
-    }
-    
+    num_objec.act = str == 'symbolAct' ? num_objec.act : null ;
     display_num.value = num_objec.current_value;
-
 }
 
-//數字輸入
+//數字輸入 input number
 function num(val)
 {
     let new_val = null
@@ -142,10 +145,9 @@ function num(val)
         update_num('next', new_val)
         display_num.value = new_val;
     }
-
 }
 
-// 數字輸入合併
+// 數字輸入合併 handle the number
 function map_num(current, val, act)
 {
     //當前數字 無 小數點數字處理
@@ -164,18 +166,20 @@ function map_num(current, val, act)
     return current.toString().split('.')[0] + '.' + current.toString().split('.')[1] + val.toString();
 }
 
-//更新數字
+//更新數字 update the number
 function update_num(target, val)
 {
-    if(target == 'current'){
+    if(target == 'current')
+    {
         num_objec.current_value = val;
     }
-    else if(target == 'next'){
+    else if(target == 'next')
+    {
         num_objec.next_value = val;
     }
 }
 
-// get 小數點幾位
+// 獲取小數點長度 get the points length
 function point_len(num)
 {
     return String(num.toString().split('.')[1] != undefined ? num.toString().split('.')[1].length : [].length);
